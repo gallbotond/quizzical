9dbbcb3d9487272c3660ebe40c09550c26e2fb79
Gáll Botond <gallbotondofficial@gmail.com>
2022-02-13 06:30:08 +0200

    setting up the project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Quizzical from './components/Quizzical'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Quizzical />
   </React.StrictMode>,
   document.getElementById('root')
 );
